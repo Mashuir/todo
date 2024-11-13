252fd5ef9dbdd826ed3656648dfe37768e2c7b2e
Md.Mashiur Rahman <mashuir3@gmail.com>
2024-11-13 15:44:09 +0600

    Initial commit

---
+++ lib/app/todo_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/home_screen.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
